5cfb21de488d6d65afb0e29061ff1a09f5bf8168
Florent Monnier <monnier.florent@gmail.com>
2020-11-19 21:34:23 +0100

    use ocamlfind query for path finding in examples/katamari.sh
    and use opam for installing libs dependency

--- examples/katamari.sh
+++ examples/katamari.sh
@@ -1,8 +1,14 @@
+opam install extlib
+opam install ocamlsdl
+opam install lablgl
+
+# -I $(ocamlfind query ode)
+
 ocamlopt.opt \
   bigarray.cmxa unix.cmxa \
-  -I +sdl sdl.cmxa \
-  -I +extlib extLib.cmxa \
-  -I +lablGL lablgl.cmxa lablglut.cmxa \
-  -I +ode -I ../src ode.cmxa \
+  -I $(ocamlfind query sdl) sdl.cmxa \
+  -I $(ocamlfind query extlib) extLib.cmxa \
+  -I $(ocamlfind query lablgl) lablgl.cmxa lablglut.cmxa \
+  -I ../src ode.cmxa \
   katamari.ml \
   -o katamari.opt
